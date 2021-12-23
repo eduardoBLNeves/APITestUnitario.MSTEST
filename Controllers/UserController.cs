c2e6f37ae5aacbf7aa98391327d303b4b4a6333f
Eduardo de Barros <brhashtagbr@gmail.com>
2021-12-23 02:18:42 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using APITestUnitario.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITestUnitario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            var usuarioBD = new User();
+
+            if (usuarioBD.Nome == user.Nome &&
+                usuarioBD.Idade == user.Idade
+               )
+                return Ok();
+
+            return BadRequest();
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace APITestUnitario.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            Nome = "Eduardo";
+            Idade = 19;
+        }
+
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
